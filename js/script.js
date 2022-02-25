ae2b3859f43094b18faa5313855094160f8feaec
JayJay <timothyjapolinario@gmail.com>
2022-02-25 15:56:25 +0800

    Add drawing feature

--- js/script.js
+++ js/script.js
@@ -1,24 +1,45 @@
 const paper = document.querySelector('.paper');
 const paperWidth = paper.clientWidth;
 const paperHeight = paper.clientHeight;
-
-//const paperHeight = "500px"
-//paper.setAttribute('style', `width:${paperWidth}`)
-
-console.log(paperWidth)
-console.log(paperHeight)
 let pixelCount = 16;
-let i = 0;
-let j = 0;
 let pixelWidthHeight = paperWidth/pixelCount
 let padding = 2
-while(i < pixelCount){
+let isHoldingMouse = false;
+console.log(pixelWidthHeight);
+
+setNewPaperGridSize();
+
+
+function setNewPaperGridSize(){
+    let i = 0;
+    while(i < pixelCount * pixelCount){
+        const pixel = createPixel()
+        paper.append(pixel);
+        i++;
+    }
+    paper.setAttribute('style', `grid-template-columns:repeat(${pixelCount},${pixelWidthHeight}fr); grid-auto-rows: ${pixelWidthHeight}px`)    
+}
+
+function createPixel(){
     const div = document.createElement('div');
     div.setAttribute('class', 'pixel');
     div.setAttribute('style', `height:${pixelWidthHeight-padding}px; width:${pixelWidthHeight-padding}px;`)
-    paper.append(div);
-    i++;
+    div.addEventListener('mouseover', ()=>drawPixel(div));
+    return div;
 }
 
-paper.setAttribute('style', `grid-template-columns:repeat(${pixelCount},${pixelWidthHeight}fr);`)
+function drawPixel(event){
+    if(isHoldingMouse){
+        event.style.background = "black";
+    }
+}
+
+window.addEventListener('mousedown', () => {
+    isHoldingMouse = true;
+    console.log(isHoldingMouse);
+})
+window.addEventListener('mouseup', () => {
+    isHoldingMouse = false;
+    console.log(isHoldingMouse);
+})
 
